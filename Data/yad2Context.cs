00d4619e34922495cfdc032b9b0249dfa7b037ff
talH21 <taltal586@gmail.com>
2021-05-29 21:26:54 +0300

    add users table

---
+++ Data/yad2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using yad2.Models;
+
+namespace yad2.Data
+{
+    public class yad2Context : DbContext
+    {
+        public yad2Context (DbContextOptions<yad2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<yad2.Models.User> User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace yad2.Models
+{
+    public class User
+    {
+        [Key]
+        public String Username { get; set; }
+        [Required]
+        public String Password { get; set; }
+        public String Email { get; set; }
+        public int Phone { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
