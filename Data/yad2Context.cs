4f609f0745ad948df8a56468a63b94e1c9b3ad94
talH21 <taltal586@gmail.com>
2021-06-12 18:15:55 +0300

    add product model with relations

--- Data/yad2Context.cs
+++ Data/yad2Context.cs
@@ -15,5 +15,30 @@ namespace yad2.Data
         }
 
         public DbSet<yad2.Models.User> User { get; set; }
+        public DbSet<yad2.Models.Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Init base class
+            base.OnModelCreating(modelBuilder);
+
+
+            // Set relationsships between our DB tables
+           /* modelBuilder.Entity<Models.User>()
+                .HasMany(b => b.Posts)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Models.Product>()
+                .HasOne(p => p.Post)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Models.Post>()
+                .HasOne(p => p.Product)
+                .WithMany(p => p.Tags);
+
+            modelBuilder.Entity<Models.Tag>()
+                .HasMany(p => p.posts)*/
+           
+        }
     }
 }
---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yad2.Models
+{
+    public class Product
+    {
+        [Key]
+        public String ProductId { set; get; }
+
+        public String description { set; get; }
+        public int price { set; get; }
+
+        // remove after add post model
+        /*public Post post { set; get; }*/
+    }
+}
