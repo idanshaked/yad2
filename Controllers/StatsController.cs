93018354ff81432c7e16d2f0708dff2526306566
idanshaked <id.shaked@gmail.com>
2021-06-19 17:38:50 +0300

    init design and getting dummy data from server

---
+++ Controllers/StatsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using yad2.Data;
+
+namespace yad2.Controllers
+{
+    public class StatsController : Controller
+    {
+        private readonly yad2Context _context;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult PostsGroupByTags()
+        {
+            return Json("[300, 500, 100]");
+        }
+    }
+}
