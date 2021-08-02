772614b1159eabaf6e8176c880db7c3705f957ff
idanshaked <id.shaked@gmail.com>
2021-08-02 23:44:01 +0300

    pie and bars with real data

--- Controllers/StatsController.cs
+++ Controllers/StatsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using yad2.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace yad2.Controllers
 {
@@ -12,26 +13,41 @@ namespace yad2.Controllers
     {
         private readonly yad2Context _context;
 
+        public StatsController(yad2Context context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public JsonResult PostsGroupByTags()
+        public JsonResult ProductsByStores()
         {
-            return Json("[300, 500, 100]");
+            var query = _context.Products
+                .GroupBy(product => product.store.storeName)
+                .Select(p => new { storeName = p.Key, count = p.Count() });
+            return Json(query.ToList());
         }
 
         [HttpGet]
         public JsonResult PostsByDate()
         {
-            //return Json("[{'amount':300,'date':'01/08/21'},{'amount':300,'date':'01/08/21'}]");
-            return Json(new[] {
-                new { amount = 15, date = "29/07/21" },
-                new { amount = 300, date = "01/08/21" }, 
-                new { amount = 100, date = "02/08/21" } 
-            });
+            var query = _context.Posts
+                   .GroupBy(p => new { d = p.PublishDate.Day, m = p.PublishDate.Month, y = p.PublishDate.Year })
+                   .Select(g => new { 
+                       date = g.Key.d + "." + g.Key.m + "." + g.Key.y, 
+                       count = g.Count() });
+
+            //return Json(new[] {
+            //  new { amount = 15, date = "29/07/21" },
+            //new { amount = 300, date = "01/08/21" }, 
+            //new { amount = 100, date = "02/08/21" } 
+            //});
+
+            return Json(query.ToList());
         }
     }
 }
