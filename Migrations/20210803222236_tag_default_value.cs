34c2f7f3e724279616c81795b30469018c5c7efa
talH21 <taltal586@gmail.com>
2021-08-04 23:49:27 +0300

    fix user and stores edit

---
+++ Migrations/20210803222236_tag_default_value.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace yad2.Migrations
+{
+    public partial class tag_default_value : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "tagIcon",
+                table: "Tags",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldDefaultValue: "fas fa-question");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "tagIcon",
+                table: "Tags",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "fas fa-question",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
